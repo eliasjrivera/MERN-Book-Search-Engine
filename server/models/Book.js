b6cd7245fca9749647dd189bcc5c732e628614f2
Elias Rivera <eliasjohnrivera@gmail.com>
2023-09-05 16:17:55 -0700

    refactored server side code

--- server/models/Book.js
+++ server/models/Book.js
@@ -1,23 +1,18 @@
-const mongoose = require('mongoose');
-const { Schema } = mongoose;
+const { Schema } = require('mongoose');
 
 // This is a subdocument schema, it won't become its own model but we'll use it as the schema for the User's `savedBooks` array in User.js
 const bookSchema = new Schema({
-    // saved book id from GoogleBooks
-    bookId: {
-        type: String,
-        required: true,
-    },
     authors: [
         {
-        type: String,
+            type: String,
         },
     ],
     description: {
         type: String,
         required: true,
     },
-    title: {
+    // saved book id from GoogleBooks
+    bookId: {
         type: String,
         required: true,
     },
@@ -27,8 +22,10 @@ const bookSchema = new Schema({
     link: {
         type: String,
     },
+    title: {
+        type: String,
+        required: true,
+    },
 });
 
-const Book = model('Book', bookSchema);
-
-module.exports = Book;
+module.exports = bookSchema;
