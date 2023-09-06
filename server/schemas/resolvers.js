06d2db9844fd00b51030908646edf2de8c0081ed
Elias Rivera <eliasjohnrivera@gmail.com>
2023-09-05 19:37:26 -0700

    up to date refactored code

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,5 +1,6 @@
-const { signToken, AuthenticationError } = require('../utils/auth');
+const { AuthenticationError } = require('apollo-server-express');
 const { User } = require('../models');
+const { signToken } = require('../utils/auth');
 
 const resolvers = {
     Query: {
